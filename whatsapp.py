9766ca9f87c5ba41b8d319c048fe97db8db58663
DaithiShan <david.shanahan.burns@gmail.com>
2021-08-22 20:32:25 +0000

    WhatsApp function created to replace sms.py

---
+++ whatsapp.py
@@ -0,0 +1,17 @@
+import os
+import env
+from twilio.rest import Client
+
+ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+NOTIFY_SERVICE_SID = os.getenv('TWILIO_NOTIFY_SERVICE_SID')
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+message = client.messages.create( 
+                              from_='whatsapp:+14155238886',  
+                              body='Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/',      
+                              to='whatsapp:+353851686169' 
+                          ) 
+ 
+print(message.sid)
